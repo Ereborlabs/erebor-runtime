92d8f9b39b5dfea55851e6c587f1c974deb210f3
Navid Haghighat <navid.haghighat@gmail.com>
2026-09-23 23:55:36 -0400

    Remove obsolete network peer export

--- crates/mithril-e2e/src/lib.rs
+++ crates/mithril-e2e/src/lib.rs
@@ -30,7 +30,6 @@ pub use capability::{CompileRecordV1, PlatformProbeV1};
 pub use closure::ClosureLedgerV1;
 pub use digest::DigestV1;
 pub use discovery::{run_discovery_offline, DiscoveryQualificationRunner};
-pub use effect::run_network_peer_server;
 pub use effect::{
     run_effect_child, run_mount_reconfigure_child, run_mount_setattr_child, EffectHealthV1,
     EffectPhysicalProbeBundleV1, EffectTestRunner, HfStaticEffectClassificationCaseV1,
